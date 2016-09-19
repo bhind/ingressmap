3c4b18751b8f5021461b657b8b3fb71b9049ec96
bhind <bhind@bhind-no-MacBook-Pro.local>
2016-09-20 01:31:05 +0900

    fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/ingressmap.html', function(req, res) {
+app.get('/', function(req, res) {
   res.render('ingressmap', { clientId: process.env.GMAIL_CLIENT_ID });
 });
 
