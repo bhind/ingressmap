a8c35906865a61ace8384fe66713f288fa78e5c7
bhind <bhind@bhind-no-MacBook-Pro.local>
2016-09-20 01:19:51 +0900

    fixed

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-console.log('coco');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
