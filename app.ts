343cc1a99d623cc97e0eac54c022d035cf13a40d
bhind <bhind@bhind-no-MacBook-Pro.local>
2016-09-26 01:44:16 +0900

    introduce @types type management

--- app.ts
+++ app.ts
@@ -1,20 +1,26 @@
 /**
  * Created by bhind on 9/24/16.
  */
-var express = require('express');
-var stylus = require('stylus');
-var nib = require('nib');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var app = express();
+import express = require('express');
+import stylus = require('stylus');
+import nib = require('nib');
+import path = require('path');
+import favicon = require('serve-favicon');
+import logger = require('morgan');
+import cookieParser = require('cookie-parser');
+import bodyParser = require('body-parser');
+
+import routes = require('./routes/index');
+import users = require('./routes/users');
+
+let app = express();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(stylus.middleware({ src: __dirname + '/views', dest: __dirname + '/public', compile: compile }));
+
+app.use(stylus.middleware({src: __dirname + '/views', dest: __dirname + '/public', compile: compile}));
+
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,25 +28,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', routes);
 app.use('/users', users);
-app.get('/ingressmap.html', function (req, res) {
+
+app.get('/ingressmap.html', function(req, res) {
     res.render('ingressmap', {
         clientId: process.env.GMAIL_CLIENT_ID,
         mapApiKey: process.env.GOOGLE_MAPS_API_KEY
     });
 });
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+app.use(function(req, res, next) {
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
+
 // error handlers
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,16 +59,20 @@ if (app.get('env') === 'development') {
         });
     });
 }
+
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
+
+
 module.exports = app;
+
 function compile(str, path) {
     return stylus(str)
         .set('filename', path)
